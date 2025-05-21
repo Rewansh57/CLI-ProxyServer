702527f294480fbe95140701e29595cbe65b373c
Rewansh <rewansh57@gmail.com>
2025-05-21 17:41:24 +0530

    Video tutorial

---
+++ src/main/java/org/example/springsecuritylearning2/videotut/VideotutApplication.java
@@ -0,0 +1,13 @@
+package org.example.springsecuritylearning2.videotut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideotutApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideotutApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springsecuritylearning2/videotut/VideotutApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springsecuritylearning2.videotut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideotutApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
