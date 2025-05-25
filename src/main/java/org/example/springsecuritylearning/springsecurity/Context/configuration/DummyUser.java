d5f382e60cf02e762ee9d7f130be2a06c2cc6f13
Rewansh <rewansh57@gmail.com>
2025-05-25 23:38:52 +0530

    CmmandLine operations

--- src/main/java/org/example/springsecuritylearning/springsecurity/Context/configuration/DummyUser.java
+++
@@ -1,8 +0,0 @@
-package org.example.springsecuritylearning.springsecurity.Context.configuration;
-
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import java.lang.module.ModuleDescriptor;
-import java.util.List;
-
---
+++ src/main/java/org/example/springsecuritylearning/springsecurity/Context/runner/CommandLineRunner.java
@@ -0,0 +1,33 @@
+package org.example.springsecuritylearning.springsecurity.Context.runner;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.env.Environment;
+
+public class CommandLineRunner implements ApplicationRunner {
+    @Value("${app.name:Username}")
+    private String username;
+
+    private Environment env;
+
+    public CommandLineRunner(Environment env){
+        this.env=env;
+
+    }
+    public void run(ApplicationArguments args){
+        args.getNonOptionArgs().forEach((o)->System.out.println("NonoptionArgs:"+o));
+
+        if (args.containsOption("mode")){
+            System.out.println("mode:"+args.getOptionValues("mode").get(0));
+
+        }
+      System.out.println("username " +username);
+        System.out.println(env.getActiveProfiles());
+
+
+
+
+    }
+
+}
